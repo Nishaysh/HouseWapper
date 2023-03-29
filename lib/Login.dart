5fd7a5d0e446440066f8dc07bbb722a86ee1570e
Your Name <Houseswaper@gmail.com>
2023-03-29 23:36:47 +0500

    firebase setup

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,8 +1,5 @@
-
-
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:property_swap/Matches.dart';
 import 'SignUp.dart';
 
 import 'package:get/get.dart';
@@ -26,59 +23,58 @@ class _LoginState extends State<Login> {
             children: [
               Container(
                 width: W,
-                height: H*0.3,
+                height: H * 0.3,
                 decoration: const BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage('img/3.jpeg'),
                   ),
                 ),
               ),
-                SizedBox(height: H*0.02),
+              SizedBox(height: H * 0.02),
               Container(
                 margin: const EdgeInsets.only(left: 20, right: 20),
                 width: W,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                   const Text('House Swap',
-                   style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                      ), 
+                    const Text(
+                      'House Swap',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-        
-                    SizedBox(height: H*0.02),
-                    
-                   const Text('Welcome to House Swap',
-                   style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                      ), 
+                    SizedBox(height: H * 0.02),
+                    const Text(
+                      'Welcome to House Swap',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                     
-                     SizedBox(height: H*0.015),
-        
+                    SizedBox(height: H * 0.015),
                     Container(
                       decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                       color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
                         boxShadow: [
                           BoxShadow(
                             blurRadius: 10,
                             spreadRadius: 0.7,
-                            offset: const Offset(1,1),
-                           color: Colors.grey.withOpacity(0.5),
+                            offset: const Offset(1, 1),
+                            color: Colors.grey.withOpacity(0.5),
                           ),
-                      ],
-                      
+                        ],
                       ),
                       child: TextField(
                         decoration: InputDecoration(
                           hintText: 'Email',
-                          suffixIcon: const Icon(Icons.email,color: Colors.red,),
-                          
+                          suffixIcon: const Icon(
+                            Icons.email,
+                            color: Colors.red,
+                          ),
                           focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(30),
                             borderSide: const BorderSide(
@@ -90,27 +86,29 @@ class _LoginState extends State<Login> {
                           ),
                         ),
                         keyboardType: TextInputType.emailAddress,
-                        
                       ),
                     ),
-                  SizedBox(height: H*0.015),
-                  Container(
+                    SizedBox(height: H * 0.015),
+                    Container(
                       decoration: BoxDecoration(
-                         borderRadius: BorderRadius.circular(30),
+                        borderRadius: BorderRadius.circular(30),
                         color: Colors.white,
                         boxShadow: [
                           BoxShadow(
                             blurRadius: 10,
                             spreadRadius: 0.7,
-                            offset: const Offset(1,1),
-                           color: Colors.grey.withOpacity(0.5),
+                            offset: const Offset(1, 1),
+                            color: Colors.grey.withOpacity(0.5),
                           ),
                         ],
                       ),
                       child: TextField(
                         decoration: InputDecoration(
                           hintText: 'Password',
-                          suffixIcon: const Icon(Icons.password,color: Colors.red,),
+                          suffixIcon: const Icon(
+                            Icons.password,
+                            color: Colors.red,
+                          ),
                           focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(30),
                             borderSide: const BorderSide(
@@ -123,87 +121,76 @@ class _LoginState extends State<Login> {
                         ),
                         keyboardType: TextInputType.visiblePassword,
                         obscureText: true,
-                      
                       ),
                     ),
-                    SizedBox(height: H*0.015),
-
-                    
-
-                  Row(
-                    children: [
-                      Expanded(child: Container(),),
-                        
-                 const Text('Forgot Your Password',
-                   style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                      ), 
-                    ),
-        
-                    ],
-                  ),
-                  SizedBox(height: H*0.03),
-                  Center(
-                    child: Container(
-                      
-                     margin: const EdgeInsets.only(left: 110, right: 110),                
-                      width: W*0.4,
-                      height: H*0.06,
-                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                          color: Colors.blue[900],
-                          boxShadow: const [
-                            
-                          ],
-                       ),
-                       child: const Center(
-                         child: Text('Sign In',
-                         style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                      
-                         ),
+                    SizedBox(height: H * 0.015),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(),
                         ),
-                       ),
-        
+                        const Text(
+                          'Forgot Your Password',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  
-                   SizedBox(height: H*0.05),
-        
-                  Center(
-                    child: RichText(text: TextSpan(
-                      text:'Not A Member ',
-                           style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                            children:[
-                              TextSpan(
-                              text:' Register',
-                           style: const TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red),
-                            recognizer: TapGestureRecognizer()..onTap=()=>Get.to(()=>SignUp())
-                              ),
-                            ],
+                    SizedBox(height: H * 0.03),
+                    Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 110, right: 110),
+                        width: W * 0.4,
+                        height: H * 0.06,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.blue[900],
+                          boxShadow: const [],
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
-                            
-                     ),
-                     
-                  ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: H * 0.05),
+                    Center(
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Not A Member ',
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          children: [
+                            TextSpan(
+                                text: ' Register',
+                                style: const TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () => Get.to(() => SignUp())),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              
             ],
           ),
         ),
       ),
-      );
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,42 @@
-// import 'package:flutter/material.dart';
-
-// import 'package:get/get.dart';
-// import 'Login.dart';
-
-// void main() {
-//   runApp(
-//     GetMaterialApp(
-//       home: Login(),
-//     ),
-//   );
-// }
-
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+import 'package:property_swap/HomePage.dart';
+import 'package:property_swap/Matches.dart';
+import 'package:property_swap/Messaging.dart';
+import 'package:property_swap/MultiStepForm.dart';
+import 'package:property_swap/YourProperty.dart';
+import 'package:property_swap/liked.dart';
 import 'Login.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Portfolio',
+  runApp(
+    GetMaterialApp(
       home: Login(),
-    );
-  }
+    ),
+  );
 }
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// import 'Login.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'My Portfolio',
+//       home: Login(),
+//     );
+//   }
+// }
